89dbffe987fdddb6525405cba23d8532d61210a1
Ayodeji61 <alabidejimichael@gmail.com>
2022-09-16 14:11:37 -0600

    Adding MongoDB file

---
+++ mongodb.sh
@@ -0,0 +1,18 @@
+LOG_FILE=/tmp/mongodb
+echo "Setting MongoDB Repo"
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo
+echo Status = $?
+
+echo "Installing MongoDB Server"
+yum install -y mongodb-org &>>$LOG_FILE
+echo Status = $?
+
+echo "Update MongoDB Listen Address"
+sed -i -e 's/127.0.01/0.0.0.0' /etc/mongo.conf &>>$LOG_FILE
+echo Status = $?
+
+echo "Starting MongoDB Service"
+systemctl enable mongod &>>$LOG_FILE
+systemctl restart mongod &>>$LOG_FILE
+echo Status =$?
+
