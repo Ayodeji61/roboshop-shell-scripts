f7b735f9a0d7f86b931829a71958915aef048a54
Ayodeji61 <alabidejimichael@gmail.com>
2022-09-16 14:28:59 -0600

    MongoDB

--- mongodb.sh
+++ mongodb.sh
@@ -12,22 +12,22 @@ sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 echo Status = $?
 
 echo "Downloading MongoDB Schema"
-curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
+curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip" &>>$LOG_FILE
 echo Status = $?
 
 cd /tmp
 
 echo "Extracting Schema File"
-unzip mongodb.zip
+unzip mongodb.zip &>>$LOG_FILE
 echo Status = $?
 
 cd mongodb-main
 
 echo "Loading Catalogue Service Schema"
-mongo < catalogue.js
+mongo < catalogue.js &>>$LOG_FILE
 echo Status = $?
 echo "Loading Users Service Schema"
-mongo < users.js
+mongo < users.js &>>$LOG_FILE
 echo Status = $?
 
 
