4f408c26e7e2f70ebd9fb6a3ffdc19ba58844d34
Ayodeji61 <alabidejimichael@gmail.com>
2022-09-16 14:18:34 -0600

    MongoDB

--- mongodb.sh
+++ mongodb.sh
@@ -1,6 +1,6 @@
 LOG_FILE=/tmp/mongodb
 echo "Setting MongoDB Repo"
-curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>$LOG_FILE
 echo Status = $?
 
 echo "Installing MongoDB Server"
@@ -8,7 +8,7 @@ yum install -y mongodb-org &>>$LOG_FILE
 echo Status = $?
 
 echo "Update MongoDB Listen Address"
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongo.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 echo Status = $?
 
 echo "Starting MongoDB Service"
