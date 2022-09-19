4ea912cc9c325c0e3bd2144df8fad024d4b728c1
Ayodeji61 <alabidejimichael@gmail.com>
2022-09-19 14:03:43 -0600

    Updating roboshop-shell-scripts

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -5,11 +5,11 @@ source common.sh
 LOG_FILE=/tmp/${COMPONENT}
 
 echo "Setup RabbitMQ Repos"
-curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>>$LOG_FILE
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>>$LOG_FILE
 StatusCheck $?
 
 echo "Install Erland & RabbitMQ"
-yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y &>>$LOG_FILE
+yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm rabbitmq-server -y &>>$LOG_FILE
 StatusCheck $?
 
 
