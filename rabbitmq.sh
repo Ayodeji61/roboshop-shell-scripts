fa465cf99020704504b04c837c1e29a3a8ed4321
Ayodeji61 <alabidejimichael@gmail.com>
2022-09-19 14:07:04 -0600

    Updating roboshop-shell-scripts

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -18,9 +18,12 @@ systemctl enable rabbitmq-server &>>$LOG_FILE
 systemctl start rabbitmq-server &>>$LOG_FILE
 StatusCheck $?
 
-echo "Add Application User in RabbitMQ"
-rabbitmqctl add_user roboshop roboshop123 &>>$LOG_FILE
-StatusCheck $?
+rabbitmqctl list_users | grep roboshop &>>$LOG_FILE
+if [ $? -ne 0 ]; then
+  echo "Add Application User in RabbitMQ"
+  rabbitmqctl add_user roboshop roboshop123 &>>$LOG_FILE
+  StatusCheck $?
+fi
 
 echo "Add Application User tags in RabbitMQ"
 rabbitmqctl set_user_tags roboshop administrator &>>$LOG_FILE
