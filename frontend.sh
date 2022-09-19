449c5041c11406c6f439f80412185e29ba51fab7
Ayodeji61 <alabidejimichael@gmail.com>
2022-09-19 15:04:36 -0600

    Updating roboshop-shell-scripts

--- frontend.sh
+++ frontend.sh
@@ -26,7 +26,7 @@ StatusCheck $?
  StatusCheck $?
 
 echo "Update Roboshop Configuration file"
- sed -i -e '/catalogue/ s/localhost/catalogue.roboshop.internal/' -e '/user/ s/localhost/user.roboshop.internal/' -e '/shipping/ s/localhost/shipping.roboshop.internal/' e- '/cart/ s/localhost/cart.roboshop.internal/' -e '/payment/ s/localhost/payment.roboshop.internal/' /etc/nginx/default.d/roboshop.conf &>>$LOG_FILE
+ sed -i -e '/catalogue/ s/localhost/catalogue.roboshop.internal/' -e '/user/ s/localhost/user.roboshop.internal/' -e '/shipping/ s/localhost/shipping.roboshop.internal/' -e '/cart/ s/localhost/cart.roboshop.internal/' -e '/payment/ s/localhost/payment.roboshop.internal/' /etc/nginx/default.d/roboshop.conf &>>$LOG_FILE
   StatusCheck $?
 
  echo Starting Nginx Service
