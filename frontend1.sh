d32754a14c894a66b6fe8b57d693fb4b960c30da
Ayodeji61 <alabidejimichael@gmail.com>
2022-09-30 22:09:07 -0600

    Updating roboshop-shell-scripts

--- frontend1.sh
+++ frontend1.sh
@@ -1,16 +1,24 @@
 LOG_FILE=/tmp/frontend
 
+echo "Installing Nginx"
 yum install nginx -y &>>${LOG_FILE}
+
+echo "Enable Nginx"
 systemctl enable nginx &>>${LOG_FILE}
 systemctl start nginx &>>${LOG_FILE}
 
-
+echo "Download Application Code"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>${LOG_FILE}
 
 
 cd /usr/share/nginx/html
+
+echo "Remove Old Content"
 rm -rf * &>>${LOG_FILE}
+
+echo "Extracting Application Code File"
 unzip /tmp/frontend.zip &>>${LOG_FILE}
+
 mv frontend-main/static/* . &>>${LOG_FILE}
 mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>${LOG_FILE}
 
