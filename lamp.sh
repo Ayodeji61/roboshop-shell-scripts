cca4b62d5b48bdd3d0f1afa74d96d5f96320ee05
Ayodeji61 <alabidejimichael@gmail.com>
2022-10-02 12:28:29 -0600

    Updating roboshop-shell-scripts

--- lamp.sh
+++ lamp.sh
@@ -1,7 +1,14 @@
 
-
+echo "Install HTTPD"
 yum install httpd -y
 
-
+echo "Start httpd service"
 systemctl start httpd
 systemctl enable httpd
+
+echo "Install mysql"
+dnf install mysql-server -y
+
+systemctl start mysqld
+systemctl enable mysqld
+
