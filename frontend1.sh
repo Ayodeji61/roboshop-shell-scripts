f1de2cad236eca33e9797b5d8cbc779720231614
Ayodeji61 <alabidejimichael@gmail.com>
2022-10-02 11:42:33 -0600

    Updating roboshop-shell-scripts

--- frontend1.sh
+++ frontend1.sh
@@ -1,17 +1,12 @@
 LOG_FILE=/tmp/frontend
 
 echo "Installing Nginx"
-yum install nginx -y &>>${LOG_FILE}
-if [ $? -eq 0 ]; then
-  echo Status="SUCCESS"
-  else
-  echo Status="FAILURE"
-  exit 1
-fi
+yum install nginx -y &>>$LOG_FILE
+
 
 echo "Enable Nginx"
-systemctl enable nginx &>>${LOG_FILE}
-systemctl start nginx &>>${LOG_FILE}
+systemctl enable nginx &>>$LOG_FILE
+systemctl start nginx &>>$LOG_FILE
 
 echo "Download Application Code"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>${LOG_FILE}
@@ -20,13 +15,13 @@ curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/fron
 cd /usr/share/nginx/html
 
 echo "Remove Old Content"
-rm -rf * &>>${LOG_FILE}
+rm -rf * &>>$LOG_FILE
 
 echo "Extracting Application Code File"
-unzip /tmp/frontend.zip &>>${LOG_FILE}
+unzip /tmp/frontend.zip &>>$LOG_FILE
 
-mv frontend-main/static/* . &>>${LOG_FILE}
-mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>${LOG_FILE}
+mv frontend-main/static/* . &>>$LOG_FILE
+mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG_FILE
 
 
-systemctl restart nginx &>>${LOG_FILE}
+systemctl restart nginx &>>$LOG_FILE
