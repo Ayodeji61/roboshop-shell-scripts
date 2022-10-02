ea54868a7659a504139442a0224f915b02aea7fe
Ayodeji61 <alabidejimichael@gmail.com>
2022-10-02 11:58:24 -0600

    Updating roboshop-shell-scripts

--- mongodb1.sh
+++ mongodb1.sh
@@ -1,20 +1,43 @@
+LOG_FILE=/tmp/mongodb1
 
+source code.sh
 
-curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo
+echo "Setup Mongo Repo"
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo  &>>$LOG_FILE
+StatusCheck $?
 
-yum install -y mongodb-org
-systemctl enable mongod
-systemctl start mongod
+echo "Install Mongodb"
+yum install -y mongodb-org &>>$LOG_FILE
+StatusCheck $?
 
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+echo "Start and Enabling Mongo Services"
+systemctl enable mongod &>>$LOG_FILE
+systemctl start mongod &>>$LOG_FILE
+StatusCheck $?
 
+echo "Update configuration file from 127.0.0.1 to 0.0.0.0"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>$LOG_FILE
+StatusCheck $?
 
-systemctl restart mongod
+echo "Restart Services"
+systemctl restart mongod &>>$LOG_FILE
+StatusCheck $?
 
-curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
+echo "Download Appllication file"
+curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip" &>>$LOG_FILE
+StatusCheck $?
 
 cd /tmp
-unzip mongodb.zip
+echo "Extract Application file"
+unzip mongodb.zip &>>$LOG_FILE
+StatusCheck $?
+
 cd mongodb-main
-mongo < catalogue.js
-mongo < users.js
+
+echo "Load Catalogue Schema"
+mongo < catalogue.js &>>$LOG_FILE
+StatusCheck $?
+
+echo "Load User Schema"
+mongo < users.js &>>$LOG_FILE
+StatusCheck $?
